ebb0bf94614026d29d73ebc205e1015c88e31f5d
Sergii Shekhovtsov <sshekhovtsov@opera.com>
2018-12-07 15:08:01 +0100

    Bump package version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 
 setup(name='check-rabbitmq-queues',
-      version='1.1.0',
+      version='1.2.0',
       description='Package for checking current length of RabbitMQ queues.',
       author='Opera Services Team',
       author_email='svc-code@opera.com',
